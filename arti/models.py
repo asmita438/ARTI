c1e444ed968ce154e0bfac1c0116039ec39bbeef
asmita438 <37612483+asmita438@users.noreply.github.com>
2020-08-18 21:56:55 +0545

    Added the entire PDF module

--- arti/models.py
+++ arti/models.py
@@ -1,22 +1,66 @@
 from django.db import models
+from fpdf import FPDF
+from book.models import Book
 from pages.models import Page
 
+from django.conf import settings
+import os
 
-class Paragraph(models.Model):
-    text = models.TextField()
-    font_size = models.IntegerField()
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
-    paragraph_no = models.IntegerField(default=1)
-    CENTER = 'C'
-    LEFT = 'L'
-    RIGHT = 'R'
-    justification_choices = [
-        (CENTER, 'Center'),
-        (LEFT, 'Left'),
-        (RIGHT, 'Right'),
-    ]
-    justification = models.CharField(
-        max_length=6,
-        choices=justification_choices,
-        default=CENTER,
-    )
+class BookPDF(FPDF):
+    def __init__(self, book):
+        self.book = book
+        super().__init__()
+    
+    def header(self):
+        if self.page_no() == 1:
+            return
+        self.set_font('Helvetica', 'B', 14)
+        self.cell(0, 10,
+                  "Title - " + str(self.book.title), ln=1, align='L')
+    
+    def footer(self):
+        if self.page_no() == 1:
+            return
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Page number
+        self.cell(0, 10, 'Page ' + str(self.page_no()-1) + '/{nb}', 0, 0, 'C')
+    
+    def create(self):
+        self.alias_nb_pages()
+        self.add_page()
+        self.set_font('Arial','', 50)
+
+        if self.book.book_image:
+            image_location = os.path.join(settings.BASE_DIR, "media/" + self.book.book_image.name)
+            self.image(image_location, self.l_margin, 10, self.w-self.l_margin-self.r_margin, self.h-10-10)
+        x = (self.w-self.l_margin-self.r_margin)/2
+        w = self.get_string_width(self.book.title)
+        x = x-w/2
+        
+        y = 10  + 25
+        self.set_xy(x, y)
+        self.cell(0, 0, self.book.title)
+         
+        self.add_page()
+        for page in self.book.page_set.all():
+            for paragraph in page.paragraph_set.all():
+                self.set_font('Helvetica', 'B', paragraph.font_size)
+                self.multi_cell(0, paragraph.font_size/2, paragraph.text, align=paragraph.justification)
+                
+        file_location = "pdfs/{}.pdf".format(self.book.id)
+        d= settings.STATICFILES_DIRS[0]
+        output_file = os.path.join(d, file_location)
+        self.output(output_file)
+        return file_location
+
+# ~ pdf = NewBook(book_i1.d)
+# ~ pdf.alias_nb_pages()
+# ~ pdf.add_page()
+# ~ pdf.set_font('Times', '', 12)
+
+
+# ~ pdf.output('new.pdf', 'F')
+
--- arti/views.py
+++ arti/views.py
@@ -1,62 +1,11 @@
-from django.shortcuts import render
-from django.views import generic
-from paragraph.models import Paragraph
-from pages.models import Page
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from .models import BookPDF
+from book.models import Book
 
-
-
-class ParagraphList(LoginRequiredMixin, generic.ListView):
-    model = Paragraph
-    template_name = 'paragraph_list.html'
-
-
-class ParagraphForPageCreateView(LoginRequiredMixin, generic.CreateView):
-    model = Paragraph
-    fields = ['text', 'font_size', 'justification']
-    template_name = 'create.html'
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.page = Page.objects.get(id=self.kwargs['page_id'])
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-
-    def get_success_url(self):
-        return reverse_lazy('page-detail',  args=[self.kwargs['page_id']])
-
-    def form_valid(self, form):
-        if form.instance.user == self.request.user and form.instance.state in ['BT', 'VD']:
-            return super().form_valid(form)
-        return render(self.request, 'error.html')
-
-
-class ParagraphDeleteView(LoginRequiredMixin, generic.DeleteView):
-    model = Paragraph
-    template_name = 'delete.html'
-
-
-    def get_success_url(self):
-        return reverse_lazy('page-detail',  args=[self.object.page.id])
-
-    def form_valid(self, form):
-        if form.instance.user == self.request.user:
-            return super().form_valid(form)
-        
-
-class ParagraphForPageUpdateView(LoginRequiredMixin, generic.UpdateView):
-    model = Paragraph
-    fields = ['font_size', 'text', 'paragraph_no', 'justification']
-    template_name = 'create.html'
-
-    def get_success_url(self):
-        return reverse_lazy('page-detail',  args=[self.object.page.id])
-
-    def form_valid(self, form):
-        if form.instance.user == self.request.user \
-           and form.instance.state in ['BT', 'VD']:
-            return super().form_valid(form)
-        return render(self.request, 'error.html')
+def generate_pdf(self, book_id):
+    if Book.objects.filter(id=book_id).exists():
+        b = Book.objects.get(id=book_id)
+        books = BookPDF(b)
+        location = books.create()
+        print(location)
+        return redirect("/static/" + location)
